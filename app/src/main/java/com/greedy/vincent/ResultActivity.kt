337d0e33a46bd674b3444d3ed7886a4748d4b276
An nayoung <any92nabee@gmail.com>
2022-10-28 14:07:40 +0900

    login design

--- app/src/main/java/com/greedy/vincent/ResultActivity.kt
+++ app/src/main/java/com/greedy/vincent/ResultActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.greedy.vincent.databinding.ActivityMainBinding
 import com.greedy.vincent.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
@@ -20,18 +19,18 @@ class ResultActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        /* 로그인한 계정의 email 화면 출력 */
+
         binding.currentUser.text = "${auth.currentUser?.email}"
 
-        /* 로그아웃 버튼 클릭 이벤트 */
+
         binding.btnLogout.setOnClickListener {
 
-            /* 로그아웃 후 로그인 화면으로 갈 때 activity stack을 비우고 이동 */
+
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
 
-            /* firebase 서버에도 로그아웃 요청 */
+
             auth.signOut()
         }
 
